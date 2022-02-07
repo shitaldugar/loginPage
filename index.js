12a65a1bf03cada8d14faf9f50f9d2dd40561eac
shitaldugar <shitalsonawat@gmail.com>
2022-02-07 09:50:38 +0530

    updated

--- index.js
+++ index.js
@@ -8,38 +8,33 @@ function Validation(){
         return false;
     }
     
-    
-    if(email.indexOf('@')<=0){
+    if((email.indexOf('@')<=0) && (isNaN(email))){
         document.getElementById("emailid").innerHTML="**position of @ is invalid";
         return false;
     }
-      
 
-    
-    if(email.charAt(email.length-4)!='.' && (email.charAt(email.length-3)!='.')){
+    if(!isNaN(email) && (email.length!=10)){
+        document.getElementById("emailid").innerHTML="**number should be of 10 digit only";
+        return false;
+    }
+   
+    if(email.charAt(email.length-4)!='.' && (email.charAt(email.length-3)!='.') &&(isNaN(email))){
         document.getElementById("emailid").innerHTML="**position of. is invalid";
         return false;
 
     }
-
-    
     if(password==""){
         document.getElementById("pass").innerHTML="**plz fill the password";
         return false;
     }
- 
-
+    
     if((password.length<5 )||(password.length>20)){
         document.getElementById("pass").innerHTML="** paasword length should be between 6 to 20 letter";
         return false;
     }
     else {
         document.getElementById("emailid").innerHTML="";
-        document.getElementById("pass").innerHTML="";
-        alert("you have logged in successfully!")
 
+      alert("you have logged in successfully!")
     }
-    
-
-    
 }
